5a468bac42c69fef6b2272ff7890d1c485f30952
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-03-29 17:11:11 +0530

    learned about styles and fonts

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import {Text, View } from "react-native";
+import { styles } from "../styles/auth.styles";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.title}>Hello subh</Text>
+    </View>
+  );
+}
+

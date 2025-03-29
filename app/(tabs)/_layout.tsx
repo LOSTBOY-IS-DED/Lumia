8142020b7609a88f064502e019050011f851d32c
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-03-29 18:25:33 +0530

    added icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,15 @@
 import { Tabs } from "expo-router";
+import Ionicons from '@expo/vector-icons/Ionicons';
+
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-        <Tabs.Screen name="index" options={{title : "Home"}} />
-        <Tabs.Screen name="bookmarks" options={{title : "Bookmarks"}} />        
-        <Tabs.Screen name="create" options={{title : "Create"}} />
-        <Tabs.Screen name="notifications" options={{title : "Notifications"}} />
-        <Tabs.Screen name="profile" options={{title : "Profile"}} />
+    <Tabs screenOptions={{tabBarShowLabel : false}}>
+        <Tabs.Screen name="index" options={{tabBarIcon : ({size, color}) => <Ionicons name="home" size={size} />}} />
+        <Tabs.Screen name="bookmarks" options={{tabBarIcon : ({size, color}) => <Ionicons name="bookmark" size={size} />}} />       
+        <Tabs.Screen name="create" options={{tabBarIcon : ({size, color}) => <Ionicons name="add-circle" size={size} />}} />
+        <Tabs.Screen name="notifications" options={{tabBarIcon : ({size, color}) => <Ionicons name="heart" size={size} />}} />
+        <Tabs.Screen name="profile" options={{tabBarIcon : ({size, color}) => <Ionicons name="person-circle" size={size} />}} />
         
     </Tabs>
   )
